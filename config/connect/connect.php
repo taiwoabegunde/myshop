f747881404595d8e76554876afa07863b8cbda6c
benjamintaiwo <abegundetaiwo@gmail.com>
2017-06-09 12:55:39 +0100

    Initial commit: for personal hosting

--- config/connect/connect.php
+++ config/connect/connect.php
@@ -2,24 +2,12 @@
 
 /**
  * Our database credentials.
- */
-$config = array(
-  'host'		=> '',
-  'username'	=> 'root',
-  'password'	=> '', // Hidden
-  'dbname' 	=> 'andreiho_xmldb'
-) ;
+ *
 
 /**
  * Attempt connection to the database using the defined credentials.
  */
-try {
-  $db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
-}
-catch(PDOException $e)
-{
-  echo $e->getMessage();
-}
+
 
 /**
  * Set the error mode.
